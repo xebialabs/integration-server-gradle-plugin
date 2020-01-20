4b6070055a54301a4dd20a98403d0a70ab0b0e68
Eugene Sypachev <esypachev@xebialabs.com>
2020-01-20 16:35:17 +0100

    DEPL-15603 run import task

---
+++ src/main/groovy/com/xebialabs/gradle/integration/tasks/ImportDataTask.groovy
@@ -0,0 +1,71 @@
+package com.xebialabs.gradle.integration.tasks
+
+import com.xebialabs.gradle.integration.util.DbUtil
+import org.dbunit.database.DatabaseConfig
+import org.dbunit.database.DatabaseConnection
+import org.dbunit.database.IMetadataHandler
+import org.dbunit.dataset.IDataSet
+import org.dbunit.dataset.datatype.IDataTypeFactory
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder
+import org.dbunit.operation.DatabaseOperation
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.tasks.TaskAction
+
+import java.sql.Connection
+import java.sql.Driver
+
+import static com.xebialabs.gradle.integration.util.PluginUtil.PLUGIN_GROUP
+
+class ImportDataTask extends DefaultTask {
+    static NAME = "importData"
+
+    ImportDataTask() {
+        this.configure {
+            group = PLUGIN_GROUP
+            dependsOn(DownloadAndExtractDataDistTask.NAME, StartIntegrationServerTask.NAME)
+        }
+    }
+
+    private def runImport() {
+        def dbname = DbUtil.databaseName(project)
+        if (dbname == 'derby-network' || dbname == 'derby-inmemory') {
+            throw new GradleException('import job cannot be executed with Derby in network or in-memory configuration.')
+        }
+        def dbDependency = DbUtil.detectDbDependency(dbname)
+
+        Properties properties = new Properties()
+        properties.put("user", "admin")
+        properties.put("password", "admin")
+
+        Driver driver = (Driver) Class.forName(dbDependency.getDriverClass()).newInstance()
+        Connection driverConnection = driver.connect("jdbc:postgresql://localhost:5432/xldrepo", properties)
+        driverConnection.setAutoCommit(true)
+
+        DatabaseConnection connection = new DatabaseConnection(driverConnection, "public")
+        DatabaseConfig config = connection.getConfig()
+        config.setProperty(DatabaseConfig.FEATURE_CASE_SENSITIVE_TABLE_NAMES, true)
+        config.setProperty(DatabaseConfig.FEATURE_QUALIFIED_TABLE_NAMES, true)
+        config.setProperty(DatabaseConfig.PROPERTY_ESCAPE_PATTERN, dbDependency.getEscapePattern())
+
+        IDataTypeFactory dataTypeFactory = (IDataTypeFactory) Class.forName(dbDependency.getDataFactory()).newInstance()
+        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, dataTypeFactory)
+
+        String metaFactory = dbDependency.getMetaFactory()
+        if (metaFactory != null && !metaFactory.isEmpty()) {
+            IMetadataHandler metadataHandler = (IMetadataHandler) Class.forName(metaFactory).newInstance()
+            config.setProperty(DatabaseConfig.PROPERTY_METADATA_HANDLER, metadataHandler)
+        }
+
+        FlatXmlDataSetBuilder provider = new FlatXmlDataSetBuilder()
+        provider.setColumnSensing(true)
+        provider.setCaseSensitiveTableNames(true)
+        IDataSet dataSet = provider.build(new FileInputStream("${project.buildDir}/db/xld-ci-explorer-${project.xldCiExplorerDataVersion}-repository/data.xml"))
+        DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet)
+    }
+
+    @TaskAction
+    def importData() {
+        runImport()
+    }
+}
---
+++ src/main/groovy/com/xebialabs/gradle/integration/util/DbUtil.groovy
@@ -0,0 +1,93 @@
+package com.xebialabs.gradle.integration.util
+
+class DbUtil {
+
+    private DbUtil() {}
+
+    static def databaseName(project) {
+        project.hasProperty("database") ? project.property("database").toString() : "derby-inmemory"
+    }
+
+    static final DbParameters postgresParams = new DbParameters(
+        'org.postgresql:postgresql',
+        'org.postgresql.Driver',
+        "org.dbunit.ext.postgresql.PostgresqlDataTypeFactory",
+        null,
+        "\"?\""
+    )
+    static final DbParameters mysqlPararms = new DbParameters(
+        'mysql:mysql-connector-java',
+        "com.mysql.jdbc.Driver",
+        "org.dbunit.ext.mysql.MySqlDataTypeFactory",
+        "org.dbunit.ext.mysql.MySqlMetadataHandler",
+        "`?`"
+    )
+    static final DbParameters oraclePararms = new DbParameters(
+        'com.oracle:ojdbc6',
+        "oracle.jdbc.OracleDriver",
+        "org.dbunit.ext.oracle.OracleDataTypeFactory",
+        null,
+        "\"?\""
+    )
+    static final DbParameters db2Pararms = new DbParameters(
+        'com.ibm:db2jcc4',
+        "com.mysql.jdbc.Driver",
+        "org.dbunit.ext.db2.Db2DataTypeFactory",
+        "org.dbunit.ext.db2.Db2MetadataHandler",
+        "\"?\""
+    )
+    static final DbParameters mssqlPararms = new DbParameters(
+        'com.microsoft.sqlserver:mssql-jdbc',
+        "com.microsoft.sqlserver.jdbc.SQLServerDriver",
+        "org.dbunit.ext.mssql.MsSqlDataTypeFactory",
+        null,
+        "\"?\""
+    )
+    static final DbParameters derbyPararms = new DbParameters(
+        'org.apache.derby:derby',
+        null,
+        null,
+        null,
+        "\"?\""
+    )
+    static final DbParameters derbyNetworkPararms = new DbParameters(
+        'org.apache.derby:derbyclient',
+        null,
+        null,
+        null,
+        "\"?\""
+    )
+
+    static def detectDbDependency(db) {
+        switch (db) {
+            case 'postgres': return postgresParams
+            case 'oracle-xe-11g': return oraclePararms
+            case 'db2': return db2Pararms
+            case ['mysql', 'mysql-8']: return mysqlPararms
+            case 'mssql': return mssqlPararms
+            case 'derby-network': return derbyNetworkPararms
+            case 'derby-inmemory': return derbyPararms
+            default: return null
+        }
+    }
+}
+
+class DbParameters {
+    String driverDependency
+    String driverClass
+    String dataFactory
+    String metaFactory
+    String escapePattern
+
+    DbParameters(String driverDependency,
+                 String driverClass,
+                 String dataFactory,
+                 String metaFactory,
+                 String escapePattern) {
+        this.driverDependency = driverDependency
+        this.driverClass = driverClass
+        this.dataFactory = dataFactory
+        this.metaFactory = metaFactory
+        this.escapePattern = escapePattern
+    }
+}
