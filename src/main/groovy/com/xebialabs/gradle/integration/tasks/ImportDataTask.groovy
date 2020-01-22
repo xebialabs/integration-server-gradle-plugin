80907afa953f1a8cd85a07fb391d5505bd71c5d8
Eugene Sypachev <esypachev@xebialabs.com>
2020-01-22 15:29:04 +0100

    DEPL-15603 import task config for diff databases

--- src/main/groovy/com/xebialabs/gradle/integration/tasks/ImportDataTask.groovy
+++ src/main/groovy/com/xebialabs/gradle/integration/tasks/ImportDataTask.groovy
@@ -1,6 +1,8 @@
 package com.xebialabs.gradle.integration.tasks
 
+import com.typesafe.config.ConfigFactory
 import com.xebialabs.gradle.integration.util.DbUtil
+import org.apache.commons.io.IOUtils
 import org.dbunit.database.DatabaseConfig
 import org.dbunit.database.DatabaseConnection
 import org.dbunit.database.IMetadataHandler
@@ -12,11 +14,12 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.tasks.TaskAction
 
+import java.nio.charset.StandardCharsets
 import java.sql.Connection
 import java.sql.Driver
 
-import static com.xebialabs.gradle.integration.util.PluginUtil.PLUGIN_GROUP
 import static com.xebialabs.gradle.integration.util.PluginUtil.DIST_DESTINATION_NAME
+import static com.xebialabs.gradle.integration.util.PluginUtil.PLUGIN_GROUP
 
 class ImportDataTask extends DefaultTask {
     static NAME = "importData"
@@ -24,9 +27,18 @@ class ImportDataTask extends DefaultTask {
     ImportDataTask() {
         this.configure {
             group = PLUGIN_GROUP
-//            dependsOn(DownloadAndExtractDataDistTask.NAME, StartIntegrationServerTask.NAME)
             dependsOn(DownloadAndExtractDataDistTask.NAME)
         }
+    }
+
+    private def getConfiguration() {
+        def from = DbUtil.dbConfigFile(project)
+        def configFileStr = IOUtils.toString(from, StandardCharsets.UTF_8.name())
+        def dbConfig = ConfigFactory.parseString(configFileStr)
+        def username = dbConfig.getString("xl.repository.database.db-username")
+        def password = dbConfig.getString("xl.repository.database.db-password")
+        def url = dbConfig.getString("xl.repository.database.db-url")
+        return new Tuple3(username, password, url)
     }
 
     private def runImport() {
@@ -35,13 +47,13 @@ class ImportDataTask extends DefaultTask {
             throw new GradleException('import job cannot be executed with Derby in network or in-memory configuration.')
         }
         def dbDependency = DbUtil.detectDbDependency(dbname)
-
+        def dbConfig = getConfiguration()
         Properties properties = new Properties()
-        properties.put("user", "postgres")
-        properties.put("password", "demo")
+        properties.put("user", dbConfig.get(0))
+        properties.put("password", dbConfig.get(1))
 
         Driver driver = (Driver) Class.forName(dbDependency.getDriverClass()).newInstance()
-        Connection driverConnection = driver.connect("jdbc:postgresql://localhost:5432/xldrepo", properties)
+        Connection driverConnection = driver.connect(dbConfig.get(2), properties)
         driverConnection.setAutoCommit(true)
 
         DatabaseConnection connection = new DatabaseConnection(driverConnection, "public")
@@ -62,8 +74,9 @@ class ImportDataTask extends DefaultTask {
         FlatXmlDataSetBuilder provider = new FlatXmlDataSetBuilder()
         provider.setColumnSensing(true)
         provider.setCaseSensitiveTableNames(true)
-//        IDataSet dataSet = provider.build(new FileInputStream("${project.buildDir.toPath().resolve(DIST_DESTINATION_NAME).toAbsolutePath().toString()}/xld-ci-explorer-${project.xldCiExplorerDataVersion}-repository/data.xml"))
-        IDataSet dataSet = provider.build(new FileInputStream("${project.buildDir.toPath().resolve(DIST_DESTINATION_NAME).toAbsolutePath().toString()}/xld-ci-explorer-${'9.6.1-SNAPSHOT'}-repository/data.xml"))
+        def dataFile = "${project.buildDir.toPath().resolve(DIST_DESTINATION_NAME).toAbsolutePath().toString()}/xld-ci-explorer-${'9.6.1-SNAPSHOT'}-repository/data.xml"
+        //${project.xldCiExplorerDataVersion}
+        IDataSet dataSet = provider.build(new FileInputStream(dataFile))
         DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet)
     }
 
