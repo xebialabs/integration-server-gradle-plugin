18ae428850e4205bd928f10f39dd867360336e83
sishwarya <63347232+sishwarya@users.noreply.github.com>
2021-01-06 16:03:06 +0530

    Added gitlab groovy task. (#7)
    * ENG-3295 - Added gitlab groovy task.
    
    * ENG-3295 gitlab docker-compose file permission changed.
    
    * ENG-3295 - updated permission for gitlab-compose folder.
    
    * ENG-3295 updated the port.
    
    * ENG-3295 - docker file name changed and updated filler.sh file.
    
    * ENG-3295 - UPDATED docker-compose command for gitlab.
    
    * ENG-3295 - Updated docker-compose version.

---
+++ src/main/groovy/com/xebialabs/gradle/integration/tasks/gitlab/DockerComposeGitlabStopTask.groovy
@@ -0,0 +1,33 @@
+package com.xebialabs.gradle.integration.tasks.gitlab
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.TaskAction
+
+import java.nio.file.Paths
+
+import static com.xebialabs.gradle.integration.util.PluginUtil.PLUGIN_GROUP
+import static com.xebialabs.gradle.integration.util.PluginUtil.DIST_DESTINATION_NAME
+
+class DockerComposeGitlabStopTask extends DefaultTask {
+    static NAME = 'dockerComposeGitlabStop'
+
+    DockerComposeGitlabStopTask() {
+        this.group = PLUGIN_GROUP
+    }
+
+    @InputFiles
+    File getDockerComposeFile() {
+        def composeFilePath = Paths.get(
+                "${project.buildDir.toPath().resolve(DIST_DESTINATION_NAME).toAbsolutePath().toString()}"+"/docker-compose-gitlab.yml")
+        return composeFilePath.toFile()
+    }
+
+    @TaskAction
+    void run() {
+        project.exec {
+            it.executable 'docker-compose'
+            it.args '-f', getDockerComposeFile(), '-p' ,'gitlabServer', 'down'
+        }
+    }
+}
