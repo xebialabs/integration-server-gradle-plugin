2f7c8ec0defd85fae445fa361d224d9a25408a67
sishwarya <63347232+sishwarya@users.noreply.github.com>
2021-08-10 17:26:38 +0530

    fixing satelliteshutdown (#16)

--- src/main/groovy/ai/digital/integration/server/tasks/ShutdownIntegrationServerTask.groovy
+++ src/main/groovy/ai/digital/integration/server/tasks/ShutdownIntegrationServerTask.groovy
@@ -1,6 +1,8 @@
 package ai.digital.integration.server.tasks
 
+import ai.digital.integration.server.tasks.satellite.ShutdownSatelliteTask
 import ai.digital.integration.server.util.DbUtil
+import ai.digital.integration.server.util.SatelliteUtil
 import ai.digital.integration.server.util.ShutdownUtil
 import ai.digital.integration.server.util.WorkerUtil
 import ai.digital.integration.server.tasks.database.DatabaseStopTask
@@ -17,6 +19,9 @@ class ShutdownIntegrationServerTask extends DefaultTask {
         def dependencies = []
         if (WorkerUtil.hasWorkers(project)) {
             dependencies.push(ShutdownWorkersTask.NAME)
+        }
+        if (SatelliteUtil.hasSatellites(project)) {
+            dependencies.push(ShutdownSatelliteTask.NAME)
         }
         group = PLUGIN_GROUP
         if (DbUtil.isDerby(project)) {
