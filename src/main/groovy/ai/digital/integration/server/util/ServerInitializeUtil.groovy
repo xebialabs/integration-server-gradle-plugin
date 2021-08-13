9e09c2b6912ba9e7f1d2cb10df583e629945f9a5
bnechyporenko <bogdannechiporenko@gmail.com>
2021-08-13 10:25:10 +0200

    wip

--- src/main/groovy/ai/digital/integration/server/util/ServerInitializeUtil.groovy
+++ src/main/groovy/ai/digital/integration/server/util/ServerInitializeUtil.groovy
@@ -8,7 +8,7 @@ class ServerInitializeUtil {
     private static void createFolders(Project project) {
         project.logger.lifecycle("Preparing server destination folders.")
 
-        ["centralConfiguration", "conf", "plugins"].each { String folderName ->
+        ["centralConfiguration", "conf", "hotfix/plugins", "hotfix/lib", "plugins"].each { String folderName ->
             def folderPath = "${ServerUtil.getServerWorkingDir(project)}/${folderName}"
             new File(folderPath).mkdirs()
             project.logger.lifecycle("Folder $folderPath has created.")
