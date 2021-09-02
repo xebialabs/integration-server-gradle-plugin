45c293c4dcc33b5628c06927d3356b047e2db822
bnechyporenko <bogdannechiporenko@gmail.com>
2021-09-02 15:50:37 +0200

    Added a content for a blog "How to run a simple integration test"

---
+++ docs/assets/js/814f3328.2905dde1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"How to run a simple integration test","permalink":"/integration-server-gradle-plugin/blog/2021/09/02/run-integration-test"}]}')}}]);
--- docs/assets/js/814f3328.b1d51c16.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"run-integration-test","permalink":"/integration-server-gradle-plugin/blog/2021/09/01/run-integration-test"}]}')}}]);
