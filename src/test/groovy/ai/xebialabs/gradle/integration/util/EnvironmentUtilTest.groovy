ca6be2b14ad1a0edb26606484f90394b32d865b9
bnechyporenko <bogdannechiporenko@gmail.com>
2021-09-07 08:08:01 +0200

    wip

--- src/test/groovy/ai/xebialabs/gradle/integration/util/EnvironmentUtilTest.groovy
+++ src/test/groovy/ai/xebialabs/gradle/integration/util/EnvironmentUtilTest.groovy
@@ -13,31 +13,28 @@ class EnvironmentUtilTest {
         Map<String, String> envMap = EnvironmentUtil.getEnv(
                 "DEPLOYIT_SERVER_OPTS", false, null, null)
         assertEquals(1, envMap.size())
-        assertEquals("-Xmx1024m -Djava.util.logging.manager=org.jboss.logmanager.LogManager",
+        assertEquals("-Xmx1024m",
                 envMap.get("DEPLOYIT_SERVER_OPTS"))
 
         // environment with variableName and debugPort
         envMap = EnvironmentUtil.getEnv(
                 "DEPLOYIT_SERVER_OPTS", false, 5005, null)
         assertEquals(1, envMap.size())
-        assertEquals("-Xmx1024m -Djava.util.logging.manager=org.jboss.logmanager.LogManager " +
-                "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 ",
+        assertEquals("-Xmx1024m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 ",
                 envMap.get("DEPLOYIT_SERVER_OPTS"))
 
         // environment with variableName, debugSuspend and debugPort
         envMap = EnvironmentUtil.getEnv(
                 "DEPLOYIT_SERVER_OPTS", true, 5005, null)
         assertEquals(1, envMap.size())
-        assertEquals("-Xmx1024m -Djava.util.logging.manager=org.jboss.logmanager.LogManager " +
-                "-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 ",
+        assertEquals("-Xmx1024m -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 ",
                 envMap.get("DEPLOYIT_SERVER_OPTS"))
 
         // environment with variableName and logFileName
         envMap = EnvironmentUtil.getEnv(
                 "DEPLOYIT_SERVER_OPTS", false, null, "deployit.log")
         assertEquals(1, envMap.size())
-        assertEquals(
-                "-Xmx1024m -DLOGFILE=deployit.log -Djava.util.logging.manager=org.jboss.logmanager.LogManager",
+        assertEquals("-Xmx1024m -DLOGFILE=deployit.log",
                 envMap.get("DEPLOYIT_SERVER_OPTS"))
     }
 }
