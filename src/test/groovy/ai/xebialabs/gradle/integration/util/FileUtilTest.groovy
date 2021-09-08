3e8497e126e0d98f8835823a00a2fc3ed83aad10
bnechyporenko <bogdannechiporenko@gmail.com>
2021-09-08 09:24:04 +0200

    Updated FileUtilTest

--- src/test/groovy/ai/xebialabs/gradle/integration/util/FileUtilTest.groovy
+++ src/test/groovy/ai/xebialabs/gradle/integration/util/FileUtilTest.groovy
@@ -3,6 +3,8 @@ package ai.xebialabs.gradle.integration.util
 import ai.digital.integration.server.util.FileUtil
 import org.apache.commons.io.FileUtils
 import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -12,10 +14,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue
 
 class FileUtilTest {
 
+    final Logger logger = LoggerFactory.getLogger(FileUtilTest.class)
+
     @Test
     void copyFileTest() {
         File sourceFile = new File(FileUtilTest.class.classLoader.getResource("centralConfiguration/deploy-server.yaml").getFile())
-        String destPath =  "build/resources/test/copyTest"
+        String destPath = new File("build/resources/test/copyTest").getAbsolutePath()
+
+        logger.info("Source file path is: ${sourceFile.getAbsolutePath()}")
+        logger.info("Destination file path is: $destPath")
+        new File(destPath).mkdirs()
+
         FileUtil.copyFile(new FileInputStream(sourceFile), Paths.get(destPath + "/deploy-server-copied.yaml"))
         File destFile = new File(destPath + "/deploy-server-copied.yaml")
         assertTrue(destFile.exists())
