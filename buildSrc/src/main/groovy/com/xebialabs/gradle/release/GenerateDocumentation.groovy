0aa53fa8a953b2202d7e4ca311943a7474a89d26
bnechyporenko <bogdannechiporenko@gmail.com>
2021-09-30 08:55:12 +0200

    wip

---
+++ buildSrc/src/main/groovy/com/xebialabs/gradle/release/GenerateDocumentation.groovy
@@ -0,0 +1,25 @@
+package com.xebialabs.gradle.release
+
+import org.gradle.api.Action
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+import org.gradle.process.ExecSpec
+
+class GenerateDocumentation extends DefaultTask {
+
+    @TaskAction
+    void doRelease() {
+        getProject().exec(new Action<ExecSpec>() {
+            @Override
+            void execute(ExecSpec execSpec) {
+                project.logger.lifecycle("Generating documentation from markdown files")
+
+                execSpec.executable('./gradlew')
+                execSpec.args('commitChanges',
+                        "-PgitBranchName=master",
+                        "-PgitMessage=\"Regenerated a documentation\"",
+                        "-PgitFileContent=\"docs/*\"")
+            }
+        })
+    }
+}
