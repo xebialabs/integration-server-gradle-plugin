42a8cf88663425ed0c2a6e9da2a819dabb4fd667
bnechyporenko <bogdannechiporenko@gmail.com>
2021-10-03 21:30:20 +0200

    Migrated some classes to Kotlin

--- src/main/kotlin/ai/digital/integration/server/util/IdUtil.kt
+++ src/main/kotlin/ai/digital/integration/server/util/IdUtil.kt
@@ -8,8 +8,8 @@ open class IdUtil {
     companion object {
 
         @JvmStatic
-        fun shortId() {
-            RandomStringUtils.random(SHORT_ID_LENGTH, "0123456789abcdef")
+        fun shortId(): String {
+            return RandomStringUtils.random(SHORT_ID_LENGTH, "0123456789abcdef")
         }
     }
 }
