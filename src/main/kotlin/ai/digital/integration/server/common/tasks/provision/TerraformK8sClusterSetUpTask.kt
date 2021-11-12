1887e6708b7f4b28fd3c68d9d95d1e1e8ddaceba
egeaksoz <egeaksoz@hotmail.com>
2021-11-12 18:49:34 +0100

    changes after pr review

--- src/main/kotlin/ai/digital/integration/server/common/tasks/provision/TerraformK8sClusterSetUpTask.kt
+++ src/main/kotlin/ai/digital/integration/server/common/tasks/provision/TerraformK8sClusterSetUpTask.kt
@@ -1,15 +1,15 @@
 package ai.digital.integration.server.common.tasks.provision
 
 import ai.digital.integration.server.common.constant.PluginConstant.PLUGIN_GROUP
-import ai.digital.integration.server.common.util.TerraformUtil
+import ai.digital.integration.server.common.util.TerraformHelper
 import ai.digital.integration.server.common.util.TerraformUtil.Companion.getProvider
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
-open class SetUpK8sClusterTask : DefaultTask() {
+open class TerraformK8sClusterSetUpTask : DefaultTask() {
 
     companion object {
-        const val NAME = "setUpK8sCluster"
+        const val NAME = "terraformK8sClusterSetUp"
     }
 
     init {
@@ -19,8 +19,7 @@ open class SetUpK8sClusterTask : DefaultTask() {
     @TaskAction
     fun launch(){
         project.logger.lifecycle("Setting up a kubernetes cluster on ${getProvider(project)}")
-        TerraformUtil.execute(project, listOf("init"))
-        TerraformUtil.execute(project, listOf("apply"))
+        TerraformHelper(project).launchCluster()
     }
 
 }
