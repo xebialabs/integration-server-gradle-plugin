263247ff590e3d01a14b73478ce6a14ffefd6c5c
egeaksoz <egeaksoz@hotmail.com>
2021-11-20 10:40:45 +0100

    refactoring

--- src/main/kotlin/ai/digital/integration/server/common/util/TerraformHelper.kt
+++ src/main/kotlin/ai/digital/integration/server/common/util/TerraformHelper.kt
@@ -2,16 +2,29 @@ package ai.digital.integration.server.common.util
 
 import ai.digital.integration.server.common.domain.Provider
 import ai.digital.integration.server.common.util.ProviderUtil.Companion.getFirstProvider
+import ai.digital.integration.server.common.util.ProviderUtil.Companion.getProviders
 import org.gradle.api.Project
 import java.io.File
 import java.nio.file.Path
+import java.util.*
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.memberProperties
 
 open class TerraformHelper(val project: Project) {
 
     companion object {
-        const val terraformAwsPath = "provision/aws/main.tf"
+        const val TERRAFORM_AWS_DIR_PATH = "provision/aws"
+        const val TERRAFORM_AWS_FILE_PATH = TERRAFORM_AWS_DIR_PATH + "main.tf"
+    }
+
+    private fun getResolvedTerraformPath(project: Project, relativePath: String): Path {
+        val terraformStream = {}::class.java.classLoader.getResourceAsStream(relativePath)
+        val resultComposeFilePath =
+            IntegrationServerUtil.getRelativePathInIntegrationServerDist(project, relativePath)
+        terraformStream?.let {
+            FileUtil.copyFile(it, resultComposeFilePath)
+        }
+        return resultComposeFilePath
     }
 
     private fun getTemplate(path: String): File {
@@ -19,32 +32,49 @@ open class TerraformHelper(val project: Project) {
         return resultComposeFilePath.toFile()
     }
 
-    private fun getResolvedTerraformAwsPath(): Path {
-        val template = getTemplate(terraformAwsPath)
+    private fun resolveTerraformAwsPath() {
+        val template = getTemplate(TERRAFORM_AWS_FILE_PATH)
 
         val configuredTemplate = template.readText(Charsets.UTF_8)
-            .replace("{{EKS_CLUSTER_NAME}}", getAwsProperty(project,getFirstProvider(project),"eksClusterName"))
-            .replace("{{EKS_VPC_NAME}}", getAwsProperty(project, getFirstProvider(project),"eksVpcName"))
-            .replace("{{EKS_VPC_SOURCE}}", getAwsProperty(project, getFirstProvider(project),"eksVpcSource"))
-            .replace("{{EKS_VPC_VERSION}}", getAwsProperty(project, getFirstProvider(project), "eksVpcVersion"))
-            .replace("{{EKS_SOURCE}}", getAwsProperty(project, getFirstProvider(project), "eksSource"))
-            .replace("{{EKS_VERSION}}", getAwsProperty(project, getFirstProvider(project),"eksVersion"))
-            .replace("{{EKS_CLUSTER_VERSION}}", getAwsProperty(project, getFirstProvider(project),"eksClusterVersion"))
+            .replace("{{EKS_CLUSTER_NAME}}", getValue("eksClusterName"))
+            .replace("{{EKS_VPC_NAME}}", getValue("eksVpcName"))
+            .replace("{{EKS_VPC_SOURCE}}", getValue("eksVpcSource"))
+            .replace("{{EKS_VPC_VERSION}}", getValue("eksVpcVersion"))
+            .replace("{{EKS_SOURCE}}", getValue("eksSource"))
+            .replace("{{EKS_VERSION}}", getValue("eksVersion"))
+            .replace("{{EKS_CLUSTER_VERSION}}", getValue("eksClusterVersion"))
 
         template.writeText(configuredTemplate)
-
-        return template.toPath()
     }
 
     private fun initTerraformModule() {
-        TerraformUtil.execute(project, listOf("-chdir=${getResolvedTerraformAwsPath()}","init"))
+        runCatching {
+            resolveTerraformAwsPath()
+        }.exceptionOrNull()?.let { exception ->
+
+        }
+        TerraformUtil.execute(
+            project,
+            listOf("-chdir=${getResolvedTerraformPath(project, TERRAFORM_AWS_DIR_PATH)}", "init")
+        )
     }
 
     private fun applyTerraformModule() {
-        TerraformUtil.execute(project, listOf("-chdir=${getResolvedTerraformAwsPath()}","apply"))
+        TerraformUtil.execute(
+            project,
+            listOf("-chdir=${getResolvedTerraformPath(project, TERRAFORM_AWS_DIR_PATH)}", "apply")
+        )
     }
 
-    private fun getAwsProperty(project: Project,provider: Provider, providerProperty: String): String {
+    private fun getValue(providerProperty: String): String {
+        getProviders(project).find { provider -> provider.equals("aws") }
+            ?.let { awsProvider ->
+                return getAwsProperty(project, awsProvider, providerProperty)
+            }
+            ?: throw Exception("${providerProperty} property not found")
+    }
+
+    private fun getAwsProperty(project: Project, provider: Provider, providerProperty: String): String {
         return if (project.hasProperty(providerProperty))
             project.property(providerProperty).toString()
         else
