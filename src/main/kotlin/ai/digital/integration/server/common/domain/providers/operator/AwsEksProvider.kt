3b0d02ce27baec93963b460e8b488bbc5b70406d
Ishwarya Surendrababu <sishwarya@collab.net>
2021-12-06 15:50:53 +0530

    ENG-8184 initial commit

--- src/main/kotlin/ai/digital/integration/server/common/domain/providers/operator/AwsEksProvider.kt
+++ src/main/kotlin/ai/digital/integration/server/common/domain/providers/operator/AwsEksProvider.kt
@@ -1,9 +1,33 @@
 package ai.digital.integration.server.common.domain.providers.operator
 
 import org.gradle.api.Project
+import org.gradle.api.tasks.Input
+import org.gradle.kotlin.dsl.property
 import javax.inject.Inject
 
 
 @Suppress("UnstableApiUsage")
 open class AwsEksProvider @Inject constructor(project: Project) : Provider(project) {
+
+    @Input
+    val region = project.objects.property<String>().value("us-east-1")
+
+    @Input
+    val stack = project.objects.property<String>().value("deploy-operator-itest")
+
+    @Input
+    val clusterName = project.objects.property<String>().value("deploy-operator-cluster-itest")
+
+    @Input
+    val nodeGrpName = project.objects.property<String>().value("deploy-operator-cluster-nodegrp")
+
+    @Input
+    val clusterNodeCount = project.objects.property<Int>().value(4)
+
+    @Input
+    val sshKeyName = project.objects.property<String>().value("deploy-operator-ssh-key")
+
+    @Input
+    val kubernetesVersion = project.objects.property<String>().value("1.20")
+
 }
--- src/main/kotlin/ai/digital/integration/server/deploy/internals/cluster/operator/AwsEksHelper.kt
+++ src/main/kotlin/ai/digital/integration/server/deploy/internals/cluster/operator/AwsEksHelper.kt
@@ -1,15 +1,38 @@
 package ai.digital.integration.server.deploy.internals.cluster.operator
 
+import ai.digital.integration.server.common.domain.providers.operator.AwsEksProvider
+import ai.digital.integration.server.common.domain.providers.operator.AzureAksProvider
+import ai.digital.integration.server.common.util.FileUtil
+import org.apache.tools.ant.util.ProcessUtil
 import org.gradle.api.Project
+import java.io.File
+import java.nio.file.Paths
 
 open class AwsEksHelper(val project: Project) {
 
     fun launchCluster() {
+        val awsEksProvider: AwsEksProvider = getProvider()
+        val region = awsEksProvider.region.get()
+        val sshKey = awsEksProvider.region.get()
+        ProcessUtil.executeCommand(project, "aws --region ${region} ec2 create-key-pair --key-name ${sshKey}", logOutput = true)
 
+        val awsEksClusterTemplateFile = getTemplate("operator/aws-eks/aws-eks-cluster.yaml")
+        val awsEksClusterTemplate = awsEksClusterTemplateFile.readText(Charsets.UTF_8)
+        ProcessUtil.executeCommand(project, "aws cloudformation validate-template --template-body ${awsEksClusterTemplate} ", logOutput = true)
     }
 
     fun shutdownCluster() {
+        ProcessUtil.executeCommand(project, "aws --region  ${region} ec2 delete-key-pair --key-name ${sshKey}")
+    }
 
+    private fun getTemplate(relativePath: String): File {
+        val file = File(relativePath)
+        val fileStream = {}::class.java.classLoader.getResourceAsStream(relativePath)
+        val resultComposeFilePath = Paths.get(getProviderWorkDir(), file.name)
+        fileStream?.let {
+            FileUtil.copyFile(it, resultComposeFilePath)
+        }
+        return resultComposeFilePath.toFile()
     }
 
 }
