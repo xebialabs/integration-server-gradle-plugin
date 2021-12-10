b53d5b68262c278dc82ad47eb017e2df956c6970
Jenkins CI <deployit-dev@xebialabs.com>
2021-12-10 09:35:47 +0100

    Documentation has been updated

---
+++ docs/assets/js/077193cd.6d722f66.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1766],{4190:function(e){e.exports=JSON.parse('{"permalink":"/integration-server-gradle-plugin/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
