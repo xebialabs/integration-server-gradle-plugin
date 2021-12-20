fab4deafc7ec803b96f01ee86bf2b80e9a407b69
Jenkins CI <deployit-dev@xebialabs.com>
2021-12-20 10:07:42 +0100

    Documentation has been updated

--- docs/assets/js/5441d722.e3050dc2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[6463],{5914:function(e){e.exports=JSON.parse('{"allTagsPath":"/integration-server-gradle-plugin/blog/tags","slug":"/integration-server-gradle-plugin/blog/tags/cluster-operator","name":"cluster-operator","count":4,"permalink":"/integration-server-gradle-plugin/blog/tags/cluster-operator"}')}}]);
