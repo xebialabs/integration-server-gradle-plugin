7c8f30c4c62cdc78941f4e2f60731c5414f3e72e
Vedran Pugar <vpugar@digital.ai>
2021-12-27 12:35:04 +0100

    ENG-6757 adding nexus

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,15 @@ repositories {
     maven {
         url = uri("https://oss.sonatype.org/service/local/repositories/releases/content")
     }
+    arrayOf("public", "releases").forEach {
+        maven {
+            credentials {
+                username = project.property("nexusUserName") as String
+                password =  project.property("nexusPassword") as String
+            }
+            url = uri("${project.property("nexusBaseUrl")}/repositories/${it}")
+        }
+    }
 }
 
 idea {
