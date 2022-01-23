d39d3dc61e4016f3d01884f7fd188dee9fec506f
bnechyporenko <bogdannechiporenko@gmail.com>
2022-01-23 18:36:40 +0100

    Added documentation for cluster based setup for Release and Deploy

--- src/main/kotlin/ai/digital/integration/server/common/domain/OperatorServer.kt
+++ src/main/kotlin/ai/digital/integration/server/common/domain/OperatorServer.kt
@@ -5,7 +5,7 @@ import ai.digital.integration.server.common.util.HTTPUtil
 class OperatorServer {
     var dockerImage: String? = "xebialabs/xl-deploy"
     var httpPort: Int = HTTPUtil.findFreePort()
-    var version: String? = null
     var pingRetrySleepTime: Int = 1
     var pingTotalTries: Int = 200
+    var version: String? = null
 }
