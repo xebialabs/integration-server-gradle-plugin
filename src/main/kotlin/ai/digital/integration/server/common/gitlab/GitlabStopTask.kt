f70535efec3e32f3a18a6dbbaecf8abc6b6aff8e
Ramoji Rao <ramoji@digital.ai>
2023-06-12 11:53:08 +0530

    S-91734: gitlabstop task was failing because of dependency

--- src/main/kotlin/ai/digital/integration/server/common/gitlab/GitlabStopTask.kt
+++ src/main/kotlin/ai/digital/integration/server/common/gitlab/GitlabStopTask.kt
@@ -3,6 +3,8 @@ package ai.digital.integration.server.common.gitlab
 import ai.digital.integration.server.common.constant.PluginConstant.PLUGIN_GROUP
 import ai.digital.integration.server.common.util.DockerComposeUtil
 import ai.digital.integration.server.common.util.GitlabUtil
+import ai.digital.integration.server.deploy.tasks.cli.DownloadAndExtractCliDistTask
+import ai.digital.integration.server.deploy.tasks.server.DownloadAndExtractServerDistTask
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.TaskAction
@@ -12,6 +14,7 @@ open class GitlabStopTask : DefaultTask() {
 
     init {
         this.group = PLUGIN_GROUP
+        this.mustRunAfter(DownloadAndExtractServerDistTask.NAME, DownloadAndExtractCliDistTask.NAME)
     }
 
     @InputFiles
