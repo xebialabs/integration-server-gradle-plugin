f3e8359b0e7882792d2650c4dc2eff6e84ca6b76
Karthigaiselvan <karthikslvn@gmail.com>
2025-01-07 16:16:42 +0530

    update plugin release version

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ apply(plugin = "ai.digital.gradle-commit")
 group = "com.xebialabs.gradle.plugins"
 project.defaultTasks = listOf("build")
 
-val releasedVersion = "23.3.0-${LocalDateTime.now().format(DateTimeFormatter.ofPattern("Mdd.Hmm"))}"
+val releasedVersion = "25.1.0-${LocalDateTime.now().format(DateTimeFormatter.ofPattern("Mdd.Hmm"))}"
 project.extra.set("releasedVersion", releasedVersion)
 
 repositories {
